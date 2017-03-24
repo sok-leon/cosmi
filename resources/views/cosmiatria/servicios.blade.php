3a95e350be4844c62d3000b124404d93afca9f7c
MonserMoreno <monsecarvajalm@gmail.com>
2017-03-24 14:39:22 -0600

    funciona servicios y preguntas primera fase

--- resources/views/cosmiatria/servicios.blade.php
+++ resources/views/cosmiatria/servicios.blade.php
@@ -1,5 +1,14 @@
 @extends('index')
 @section('contenido')
-<h2> Servicios </h2>
-
+<h1> Servicios </h1><br>
+<ul>
+  @foreach ($servicios as $servicios)
+    <li>
+        <h2><br>
+          {{ $servicios->nombre}}
+        </h2>
+        <p>{{ $servicios->descripcion}}</p>
+    </li>
+  @endforeach
+</ul>
 @endsection
