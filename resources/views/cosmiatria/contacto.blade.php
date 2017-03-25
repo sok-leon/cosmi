58d1c1d8519e389b4f3cafc74c7cd9d5bed2f7cd
sok-leon <theonesok@gmail.com>
2017-03-24 19:30:01 -0700

    formulario contacto

--- resources/views/cosmiatria/contacto.blade.php
+++ resources/views/cosmiatria/contacto.blade.php
@@ -3,18 +3,12 @@
       <input type="email" class="contact-input" name="mail" id="mail" value="Email"/>
       <input type="date" class="contact-input" name="fecha" id="fecha" value="fecha"/>
 
-
-
+            <select class="contact-input">
+                @foreach ($servicios as $servicios)
+                <option>{{ $servicios->nombre}}</option>
+                @endforeach
+            </select>
 
       <input type="text" class="contact-input" name="name" id="name" value="Servicio"/>
       <div class="button-link"><a href="#">Enviar</a></div>
 </div>
-
-<div class="contact-input">
-      <h2>Select</h2>
-      <select class="form-control">
-          @foreach ($servicios as $servicios)
-          <option>{{ $servicios->nombre}}</option>
-          @endforeach
-      </select>
-  </div>
--- resources/views/cosmiatria/index.blade.php
+++ resources/views/cosmiatria/index.blade.php
@@ -4,14 +4,13 @@
       <input type="text" class="contact-input" name="name" id="nombre" value="nombre"/>
       <input type="email" class="contact-input" name="mail" id="mail" value="Email"/>
       <input type="date" class="contact-input" name="fecha" id="fecha" value="fecha"/>
-      <div class="form-group">
-            <h2>Select</h2>
-            <select class="form-control">
+
+           <select class="contact-input">
                 @foreach ($servicios as $servicios)
                 <option>{{ $servicios->nombre}}</option>
                 @endforeach
             </select>
-        </div>
+
       <div class="button-link"><a href="#">Enviar</a></div>
 </div>
 @endsection
